07d7fa8d65b3a4926cd746e1fbd4e974629c6749
Swiftaim <david.dalstrom@gmail.com>
2015-11-15 13:25:19 +0100

    deleting obsolete file

--- simpleami/svc_handler.py
+++
@@ -1,7 +0,0 @@
-class SvcHandler:
-    """SvcHandler serves as a base class for connection-oriented communication classes that require a connection
-    to a host and a port.
-    """
-    def __init__(self, host, port):
-        self.host_ = host
-        self.port_ = port
