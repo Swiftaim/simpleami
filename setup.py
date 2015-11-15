21b1ea98709c676888bce848bc315abe292fbbf2
Swiftaim <david.dalstrom@gmail.com>
2015-11-15 13:13:35 +0100

    cleaning up

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='simpleami',
-      version='0.1-alpha',
+      version='0.1',
       description='Provides a simple class for connecting to the Asterisk AMI interface and issue commands.',
       url='http://github.com/swiftaim/simpleami',
       author='David Dalström',
--- simpleami/ami_svc_handler.py
+++ simpleami/ami_svc_handler.py
@@ -5,11 +5,13 @@ class AMISvcHandler:
     methods for passing commands to the AMI as well as simplifying common tasks.
     """
     def __init__(self, host, port):
+        """Provide the AMI host and port."""
         self.host_ = host
         self.port_ = port
         self.sock_ = None
 
     def connect(self, username, password):
+        """Connect by providing AMI username and password (secret)."""
         try:
             self.sock_ = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.sock_.connect((self.host_, self.port_))
